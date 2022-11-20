c722a3bb9237905cf374043b5548d079a1a4038c
kaisskhiri <116981697+kaisskhiri@users.noreply.github.com>
2022-11-20 16:42:14 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import './input.css';
+import BasicExample from './Form';
+import SelectBasicExample from './Select';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <BasicExample/>
+      
+    </div>
+  );
+}
+
+export default App;
